089bd7089e73da85d848601820a197c4b5a55dfc
ryanrbart <rrbart@gmail.com>
2016-05-13 07:51:41 -0700

    Removed grass instruction code. Should be last of initial commits

--- code/big_creek_map.R
+++ code/big_creek_map.R
@@ -1,28 +1,5 @@
 # Map for tree-shrub type conversion manuscript
 
-# ------------
-# Get GRASS data instructions
-# https://grasswiki.osgeo.org/wiki/R_statistics/spgrass6
-
-# Open grass
-# In command line, start R.
-# Load spgrass6 and raster libraries 
-library(spgrass6)
-library(raster)
-
-# Read a raster or vector file into R
-file_rast = readRAST("file.rast")
-file_vect = readVECT("file.vect")
-
-# Convert files to rasters (probably a comprable step for vectors)
-file_rast = raster(file_rast)
-save(fire_rast, file = "fire_rast.RData")
-
-# In R
-load("fire_rast.RData")
-plot(fire_rast)
-# For ggplot - Fortify?
-fortify(fire_rast)  # error
 
 # -------------
 # Implementing Grass conversion
